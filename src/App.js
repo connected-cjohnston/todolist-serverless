986a76c3cf6b85be5fbe2d5215a2705bb29da6b7
Chris Johnston <cjohnston@connected.io>
2022-09-01 10:28:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do</h1>
+      <ul>
+        <li>Solve human suffering</li>
+        <li>Implement world peace</li>
+        <li>Cure cancer</li>
+        <li>Buy Bananas</li>
+        <li>Work out basic income</li>
+        <li>Take a cool photograph</li>
+      </ul>
     </div>
   );
 }
